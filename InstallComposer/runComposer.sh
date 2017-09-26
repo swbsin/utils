bd4230eff49e297a46b54ec3f5d1557b6d93d33f
swb <swbsin@163.com>
2017-09-26 15:26:26 +0800

    Create runComposer.sh

---
+++ InstallComposer/runComposer.sh
@@ -0,0 +1,8 @@
+git clone https://github.com/hyperledger/composer-sample-networks.git
+
+# mv project
+# npm install
+# npm test
+# composer network deploy
+# composer-rest-server
+![rest-server配置文件](https://hyperledger.github.io/composer/assets/img/tutorials/developer/composer-rest-server.png)
